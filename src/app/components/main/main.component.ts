ddafa1090a7e73c13dcc49a27d86da26ab543d23
laralopez17 <laralopez219@gmail.com>
2024-10-25 20:04:26 -0300

    agregué que solo se muestren los productos cuando se seleccione una categoria, tipo de producto, etc

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -21,9 +21,10 @@ import { CommonModule } from '@angular/common';
 export class MainComponent {
 
   active: boolean = false;
-  src!: SafeResourceUrl;
   products: IProduct[] = [];
   selectedOptions: any = {};
+  hasSelection: boolean = false; // Nueva propiedad para controlar si se ha hecho una selección
+
 
   constructor(private _sanitizer: DomSanitizer) {
     this.products = DataResource.products;
@@ -36,8 +37,7 @@ export class MainComponent {
   // Maneja los cambios de selección desde el sidebar
   onSelectionChange(selectedOptions: any): void {
     this.selectedOptions = selectedOptions;
+    this.hasSelection = !!(this.selectedOptions.headingId || this.selectedOptions.categoryId || this.selectedOptions.productTypeId);
     console.log('Selección actualizada:', this.selectedOptions);
-    // Aquí puedes aplicar la lógica para filtrar productos o manejar los cambios
   }
-
 }
