66265d45949eb868a09c5a60c7186cfd87056cac
laralopez17 <laralopez219@gmail.com>
2024-12-12 11:36:37 -0300

    Cambio de lugar los archivos

---
+++ backend/Super4/target/generated-sources/wsgen/ar/edu/ubp/das/super4/services/jaxws/ObtenerPrecios.java
@@ -0,0 +1,36 @@
+
+package ar.edu.ubp.das.super4.services.jaxws;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "ObtenerPreciosRequest", namespace = "http://services.Super4.das.ubp.edu.ar/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ObtenerPreciosRequest", namespace = "http://services.Super4.das.ubp.edu.ar/")
+public class ObtenerPrecios {
+
+    @XmlElement(name = "criteria", namespace = "")
+    private String criteria;
+
+    /**
+     * 
+     * @return
+     *     returns String
+     */
+    public String getCriteria() {
+        return this.criteria;
+    }
+
+    /**
+     * 
+     * @param criteria
+     *     the value for the criteria property
+     */
+    public void setCriteria(String criteria) {
+        this.criteria = criteria;
+    }
+
+}
